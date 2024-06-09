def3b99f2d803b90e911d16523108cf705be2437
NieLi26 <se.olivaresc@gmail.com>
2024-06-09 18:16:05 -0400

    subiendo...

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   <AgregarUsuario />
+   <ul >
+     <ListarUsuarios v-for="usuario in usuarios" :key="usuario.id" :data="usuario"/>
+   </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapActions, mapState } from 'vuex';
+import AgregarUsuario from './components/AgregarUsuario.vue'
+import ListarUsuarios from './components/ListarUsuarios.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AgregarUsuario,
+    ListarUsuarios
+  },
+  data() {
+    return {
+      listaUsuarios: []
+    }
+  },
+  computed: {
+    ...mapState(["usuarios"]),
+  },
+  methods: {
+    ...mapActions(["getUsuarios"])
+  },
+  async mounted() {
+    await this.getUsuarios()
+    
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,39 @@
 import { createStore } from 'vuex'
+import { app } from '../firebaseConfig';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 export default createStore({
   state: {
+    usuarios: []
   },
   getters: {
   },
   mutations: {
+    changeUsuarios( state, data ) {
+      state.usuarios = data
+    },
+    getUsuarios( state, data ) {
+      state.usuarios = data
+    },
   },
   actions: {
+    async change( context, data ) {
+      context.commit("changeUsuarios", data)
+    },
+    async getUsuarios( context ) {
+      try {
+        const db = getFirestore(app);
+        const usuariosSnapshot = await getDocs( collection( db, 'usuarios' ) );
+        const data = usuariosSnapshot.docs.map( doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+        console.log({data});
+        context.commit("getUsuarios", data)
+      } catch (error) {
+          console.error(error);
+      }
+    }
   },
   modules: {
   }
